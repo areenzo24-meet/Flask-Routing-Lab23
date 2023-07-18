9029bb2ff7443ba3f34bf6e0f2819274e384bf21
areenzo24-meet <139681322+areenzo24-meet@users.noreply.github.com>
2023-07-18 18:34:28 +0300

    the progrees

--- Flask-Routing-Lab/app.py
+++ Flask-Routing-Lab/app.py
@@ -12,9 +12,14 @@ app = Flask(  # Create a flask app
 @app.route('/')
 def home():
     return render_template("home.html")
-@app.route('/')
-def home():
+
+@app.route('/product')
+def home2():
     return render_template("product.html")
+
+@app.route('/cart')
+def home3():
+    return render_template("cart.html")  
 
 
 # Your code should be above
